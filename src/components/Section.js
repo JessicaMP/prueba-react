3d1af387e024f29340d5bc20ca48e5ee97359ff8
JessicaMP <jessica95.mp@gmail.com>
2018-03-14 09:21:03 -0500

    Modificando

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 class Section extends Component {
     render() {
-        const hobbies = ['codear', 'comer', 'dormir'];
+        const hobbies = ['Codear', 'Leer', 'Escuchar musica', 'Comer', 'Dormir'];
         return (
             <section>
-                <center><h3>Sobre mí</h3></center>
+                <center><h3>Mis hobbies:</h3></center>
                 <ul>
                     {hobbies.map(function (hob, i) {
                         return <li key={i}><a href="#li">{hob}</a></li>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/app';
-import Header from './components/Header';
 
 render(
   <App />,
